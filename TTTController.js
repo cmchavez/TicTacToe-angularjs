8c6ba60d962bb9557663d72b32b86905a98e80ff
Cristal Chavez <cristalmchavez@yahoo.com>
2015-05-12 20:51:45 -0700

    added code to angular, angularfire, and firebase

---
+++ TTTController.js
@@ -0,0 +1,14 @@
+angular
+	.module('tttApp')
+	.controller('TTTController', TTTController);
+
+TTTController.$inject = ['$firebaseArray']
+
+function TTTController() {
+	var self = this;
+}
+
+// function GameStart($firebaseArray) {
+
+
+// }
