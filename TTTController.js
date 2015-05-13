f0216362baceb4b1c8a35cc5c9fe4e2e9409f796
Cristal Chavez <cristalmchavez@yahoo.com>
2015-05-13 16:35:16 -0700

    got clickedBox function to work and players turns

--- TTTController.js
+++ TTTController.js
@@ -4,11 +4,41 @@ angular
 
 TTTController.$inject = ['$firebaseArray']
 
-function TTTController() {
+function TTTController($firebaseArray) {
 	var self = this;
+	self.clickedBox = clickedBox;
+	self.player1 = "x";
+	self.player2 = "o";
+	self.turn = "o";
+
+		self.boxes= [
+		{box: '', select: "empty"},
+		{box: '', select: "empty"},
+		{box: '', select: "empty"},
+		{box: '', select: "empty"},
+		{box: '', select: "empty"},
+		{box: '', select: "empty"},
+		{box: '', select: "empty"},
+		{box: '', select: "empty"},
+		{box: '', select: "empty"}
+	]
+		function getBox() {
+		var ref = new Firebase("https://19431971.firebaseio.com/");
+		var box = $firebaseArray(ref);
+		return box;
+	}
+
+	function clickedBox(i) {
+		// alert(i);
+		if (self.boxes[i].box == '' && self.turn == "o") {
+		 self.boxes[i].box = "x";
+		 self.turn = "x";
+	}	else if (self.boxes[i].box == '' && self.turn == "x") {
+		self.boxes[i].box = "o";
+		self.turn = "o";
+	}  else {
+		null
+	}
 }
-
-// function GameStart($firebaseArray) {
-
-
-// }
+	
+}
