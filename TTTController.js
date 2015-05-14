c8d2f3943ffea98087594512cfb61bd22bcee4d9
Cristal Chavez <cristalmchavez@yahoo.com>
2015-05-13 21:14:54 -0700

    connected to firebase

--- TTTController.js
+++ TTTController.js
@@ -2,7 +2,7 @@ angular
 	.module('tttApp')
 	.controller('TTTController', TTTController);
 
-TTTController.$inject = ['$firebaseArray']
+TTTController.$inject = ['$firebaseArray'];
 
 function TTTController($firebaseArray) {
 	var self = this;
@@ -10,32 +10,33 @@ function TTTController($firebaseArray) {
 	self.player1 = "x";
 	self.player2 = "o";
 	self.turn = "o";
+	self.addBox= getBox();
+	
+	self.addBox.$loaded( function() {
+		if (self.addBox.length < 9) {
+			for (var i=0;i<9;i++) {
+		self.addBox.$add({box: ''});
+		}
+	}	
+});
 
-		self.boxes= [
-		{box: '', select: "empty"},
-		{box: '', select: "empty"},
-		{box: '', select: "empty"},
-		{box: '', select: "empty"},
-		{box: '', select: "empty"},
-		{box: '', select: "empty"},
-		{box: '', select: "empty"},
-		{box: '', select: "empty"},
-		{box: '', select: "empty"}
-	]
 		function getBox() {
 		var ref = new Firebase("https://19431971.firebaseio.com/");
 		var box = $firebaseArray(ref);
 		return box;
 	}
 
-	function clickedBox(i) {
-		// alert(i);
-		if (self.boxes[i].box == '' && self.turn == "o") {
-		 self.boxes[i].box = "x";
+	function clickedBox(c) {
+		
+		if (self.addBox[c].box == '' && self.turn == "o") {
+
+		 self.addBox[c].box = "x";
 		 self.turn = "x";
-	}	else if (self.boxes[i].box == '' && self.turn == "x") {
-		self.boxes[i].box = "o";
+		 self.addBox.$save(c);
+	}	else if (self.addBox[c].box == '' && self.turn == "x") {
+		self.addBox[c].box = "o";
 		self.turn = "o";
+		self.addBox.$save(c);
 	}  else {
 		null
 	}
